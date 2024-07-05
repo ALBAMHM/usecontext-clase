b97405633ed5d283efbdaa93b6fcba66123c0173
AlbaHoyo <alba.maria.hoyo@gmail.com>
2024-07-05 17:38:49 +0200

    v1

---
+++ src/Greeting.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLanguage } from './LanguageContext';
+
+const Greeting = () => {
+  const { language } = useLanguage();
+
+  const greetings = {
+    en: 'Hello!',
+    es: '¡Hola!',
+    fr: 'Bonjour!'
+  };
+
+  return (
+    <div>
+      <h2>{greetings[language]}</h2>
+    </div>
+  );
+};
+
+export default Greeting;
